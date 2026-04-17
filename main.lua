46847296b1bb8a9ad73d270499c46ca1dda20a84
Werm <Moodysnowman@outlook.com>
2026-04-17 09:56:47 -0400

    actually started the project
    idk how I'm gonna do this but I plan to use no or a limited amount of help/tutorials for this project. a calculator cant be THAT hard to make, right? knowing me I'm gonna mess it up anyway lol

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+  mathstate = "none"
+  mathtype = "none"
+  first# = 0
+  second# = 0
+  output = "no numbers to crunch"
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  love.graphics.print("CalcuLua starts here", 0 , 0)
+end
+
