334534afae4c52ce411eca3ca5ce73df5d6dc060
vombagel64 <Moodysnowman@outlook.com>
2026-04-17 21:36:37 -0400

    this took so, SO long to do. anyway WE HAVE A CURSOR NOW! AND ALSO WE HAVE A BORDERLESS WINDOW (NOT EXCITING AT ALL) AND BEST OF ALL, WE HAVE A FULLSCREEN TOGGLE! WEEEEEEEEEEEEEE IT LOOKS HORRIBLE! WEEEEEEEEEEEEE THERES NO CALCULATOR FEATURES YET!!!!!!!!!! WHY IS THIS commit message so long and loud...?

--- main.lua
+++ main.lua
@@ -1,31 +1,72 @@
 function love.load()
+  love.window.setMode(800, 600, {
+    borderless = true
+  })
+  
+  love.graphics.setBackgroundColor(0, 0.3, 0.6)
+  love.graphics.newImage("assets/cursorF.png")
+  cursor = love.mouse.newCursor("assets/cursorF.png", 9, 9)
+  love.mouse.setCursor(cursor)
+
   mathstate = "none"
   mathtype = "none"
-  first# = 0
-  second# = 0
+  first_num = 0
+  second_num = 0
   output = "no numbers to crunch"
 
-  function domath+()
-    output = first# + second#
+  function inputNum()
+    if mathstate == "first" then
+      first_num = tonumber(love.keyboard.getTextInput())
+    elseif mathstate == "second" then
+      second_num = tonumber(love.keyboard.getTextInput())
+    end
   end
 
-  function domath-()
-    output = first# - second#
+  domath = {}  
+
+  function domath.add()
+    output = first_num + second_num
   end
 
-  function domath/()
-    output = first# / second#
+  function domath.minus()
+    output = first_num - second_num
   end
 
-  function domath*()
-    output = first# * second#
+  function domath.div()
+    output = first_num / second_num
+  end
+
+  function domath.mul()
+    output = first_num * second_num
   end
 end
 
 function love.update(dt)
 end
 
-function love.draw()
-  love.graphics.print("CalcuLua starts here", 0 , 0)
+canEsc = true
+fscreen = false
+
+function love.keypressed(key)
+    if key == "f11" then
+        if fscreen == false then
+            love.window.setFullscreen(true)
+            fscreen = true
+        else
+            love.window.setFullscreen(false)
+            fscreen = false
+        end
+    end
+
+    if key == "escape" then
+        if canEsc then
+            love.event.quit()
+        end
+    end
 end
 
+function love.draw()
+  love.graphics.print("Welcome to CalcuLua Test Window!", 0 , 0)
+    love.graphics.print("This is just a window test so it doesnt do anything", 0, 10)
+    love.graphics.print("Press esc to close the window, f11 to toggle fullscreen", 0, 20)
+end
